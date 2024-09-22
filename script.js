cc7e2b72efebc1d25265c37383bc57567d1b0f9b
raunishyadav029 <120357762+raunishyadav029@users.noreply.github.com>
2024-09-23 03:32:55 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-
-// var h5timer= document.querySelector("#line1-part1 h5")
-// var grow=0
-//  setInterval(function(){
-//     if(grow<100){
-//         h5timer.innerHTML=grow++
-        
-//     }else{
-//         h5timer.innerHTML=grow        
-//     }
-// },30)
-
-var tl= gsap.timeline()
+function loadingAnimation(){
+    var tl= gsap.timeline()
 tl.from(".line h1",{
     y:150,
     stagger:0.25,
@@ -42,17 +31,42 @@ tl.to(".line h2",{
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
-    delay:3.4
+    delay:2
 })
 
 tl.from("#page1",{
     delay:0.2,
     y:1200,
     opacity:0,
-    duration:0.5,
+    duration:0.6,
     ease:Power4
 })
 
 tl.to("#loader",{
     display:"none"
+});
+tl.from("#nav",{
+    opacity:0
+   
 })
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+
+
+}
+loadingAnimation()
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y,
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+cursorAnimation()
